2732510dcfe395a5223c8e9532fbeb824cd71db8
zaehorang <zaehorang2@naver.com>
2023-07-26 01:50:24 +0900

    first commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLable: UILabel!
+    @IBOutlet weak var myButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainLable.text = "안녕하세요"
+        myButton.setTitle("버튼", for: .normal)
     }
 
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        mainLable.text = "버튼 눌림"
+        mainLable.textColor = #colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1)
+        
+        myButton.backgroundColor = UIColor.yellow
+        myButton.setTitle("Pressed", for: .normal)
+        myButton.setTitleColor(.black, for: .normal)
+    }
+    
 
 }
 
